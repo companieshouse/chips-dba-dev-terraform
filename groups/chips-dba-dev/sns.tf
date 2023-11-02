07cc5b646b43ae965db4f3e99ab2e0d1f0be9eae
DrewHarvard <aharvard@companieshouse.gov.uk>
2023-11-02 13:11:13 +0000

    Fix terraform state to match whats in main

--- groups/chips-dba-dev/sns.tf
+++ groups/chips-dba-dev/sns.tf
@@ -1,24 +1,24 @@
-resource "aws_sns_topic" "chips-dev_topic" {
-  name = "chips-dev_topic"
-}
+# resource "aws_sns_topic" "chips-dev_topic" {
+#   name = "chips-dev_topic"
+# }
 
-resource "aws_sns_topic_subscription" "chips-dev_Subscription" {
-  topic_arn = aws_sns_topic.chips-dev_topic.arn
-  for_each  = toset(["unixsupport@companieshouse.gov.uk"])
-  protocol  = "email"
-  endpoint  = each.value
+# resource "aws_sns_topic_subscription" "chips-dev_Subscription" {
+#   topic_arn = aws_sns_topic.chips-dev_topic.arn
+#   for_each  = toset(["unixsupport@companieshouse.gov.uk"])
+#   protocol  = "email"
+#   endpoint  = each.value
 
-  depends_on = [
-    aws_sns_topic.chips-dev_topic
-  ]
-}
+#   depends_on = [
+#     aws_sns_topic.chips-dev_topic
+#   ]
+# }
 
-resource "aws_sns_topic_subscription" "chips-dev_Subscriptionhttps" {
-  topic_arn = aws_sns_topic.chips-dev_topic.arn
-  protocol  = "https"
-  endpoint  = "https://companieshouse.xmatters.com/api/integration/1/functions/663f6327-04ae-46ab-8e8d-f580f695f218/triggers?apiKey=850133a0-4cae-477e-9b69-c0cbc8154d8c&recipients=unix"
+# resource "aws_sns_topic_subscription" "chips-dev_Subscriptionhttps" {
+#   topic_arn = aws_sns_topic.chips-dev_topic.arn
+#   protocol  = "https"
+#   endpoint  = "https://companieshouse.xmatters.com/api/integration/1/functions/663f6327-04ae-46ab-8e8d-f580f695f218/triggers?apiKey=850133a0-4cae-477e-9b69-c0cbc8154d8c&recipients=unix"
 
-  depends_on = [
-    aws_sns_topic.chips-dev_topic
-  ]
-}
+#   depends_on = [
+#     aws_sns_topic.chips-dev_topic
+#   ]
+# }
