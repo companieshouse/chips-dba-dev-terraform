53c97d8b64592b4b058187e50bc8555d35948d86
Chris Harris <charris1@companieshouse.gov.uk>
2024-04-08 11:28:36 +0100

    added lookup for rep db

--- groups/chips-dba-dev/data.tf
+++ groups/chips-dba-dev/data.tf
@@ -71,6 +71,10 @@ data "vault_generic_secret" "cdp_dev_data_cidrs" {
   path = "aws-accounts/network/ch-cdp-development/data-subnets"
 }
 
+data "vault_generic_secret" "chips_staging" {
+  path = "applications/${var.aws_account}-${var.aws_region}/${var.application}/db/chips-staging"
+}
+
 data "vault_generic_secret" "ec2_data" {
   path = "applications/${var.aws_account}-${var.aws_region}/${var.application}/db/ec2"
 }
